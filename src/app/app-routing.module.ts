84590be971786a68f5adacbd3058567bf5d2103e
Pascal Chatard <pchatard@aol.com>
2022-07-17 16:39:28 +0200

    Structuration du code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './features/account/account.component';
+import { LoginComponent } from './features/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'account', component: AccountComponent},
+  { path: 'account/connection', component: LoginComponent},
+  { path: '', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './features/login/login.component';
+import { AccountComponent } from './features/account/account.component';
+import { ProfilComponent } from './profil/profil.component';
+import { TransfertComponent } from './features/transfert/transfert.component';
+import { UserComponent } from './features/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AccountComponent,
+    ProfilComponent,
+    TransfertComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
